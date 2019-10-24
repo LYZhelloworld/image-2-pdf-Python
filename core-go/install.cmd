b236f23de80867fa67815b1499f1e6384143ab9a
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2019-10-24 18:55:41 +0800

    Move go implementation to another repo

--- core-go/install.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set GOPATH=%cd%
-go get github.com/jung-kurt/gofpdf
-go install image2pdf
--- core-go/install.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-export GOPATH=$PWD
-go get github.com/jung-kurt/gofpdf
-go install image2pdf
